b303da5a14f47d1d9d689538d9d25101806998e6
hansolChoi29 <ggksthf29@gmail.com>
2025-12-23 21:09:36 +0900

    init

---
+++ app/coreboard/page.tsx
@@ -0,0 +1,16 @@
+export default function CoreboardPage() {
+  return (
+    <section className="flex flex-col gap-4 p-8 ">
+      <div className="font-coraboard text-3xl text-[#0A008F]">Coreboard</div>
+      <p className="typo-body">
+        이 작품은 2020년 10월에 제 25회 부산국제영화제에 출품되었고, 윤재호
+        감독이 이 작품으로 넷팩상(아시아영화진흥기구상)을 차지하였으며, 주연배우
+        임성미가 영화의 지수와 함께 올해의 배우상을 공동 수상하였다. 또한 2021년
+        2월에는 베를린 국제 영화제에 제너레이션 부분에 초청되었다.또한 2021년
+        2월에는 베를린 국제 영화제에 제너레이션 부분에 초청되었다.또한 2021년
+        2월에는 베를린 국제 영화제에 제너레이션 부분에 초청되었다.또한 2021년
+        2월에는 베를린 국제 영화제에 제너레이션 부분에 초청되었다.
+      </p>
+    </section>
+  );
+}
---
+++ app/dogo/page.tsx
@@ -0,0 +1,9 @@
+export default function DogoPage() {
+  return (
+    <section className="gap-4 p-8 ">
+      <div>
+        <h1 className="font-dogo text-3xl text-[#221A1A]">Dogo</h1>
+      </div>
+    </section>
+  );
+}
