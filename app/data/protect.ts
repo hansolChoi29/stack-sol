1cb0744484aff8acb508bbb38bbf3ba4a381c6ea
hansolChoi29 <ggksthf29@gmail.com>
2025-12-24 20:12:19 +0900

    style: coreboard UI setup

--- app/data/protect.ts
+++ app/data/protect.ts
@@ -1,20 +1,16 @@
-export type CamkeepPost = {
+export type Post = {
   id: number;
   title: string;
   author: string;
-  createdAt: string;
-  views: number;
-  status: "트러블슈팅" | "일반";
+  status: "트러블슈팅" | "성능 개선";
   content: string;
 };
 
-export const CAMKEEPPOSTS: CamkeepPost[] = [
+export const CAMKEEPPOSTS: Post[] = [
   {
     id: 1,
     title: "JWT 인터셉터 예외 처리",
     author: "한솔",
-    createdAt: "2025-12-24",
-    views: 91,
     status: "트러블슈팅",
     content: `상세 내상세 내상세 내상세 내상세 내상세 내용...`,
   },
@@ -22,8 +18,23 @@ export const CAMKEEPPOSTS: CamkeepPost[] = [
     id: 2,
     title: "공지: 포트폴리오 구조 안내",
     author: "한솔",
-    createdAt: "2025-12-23",
-    views: 312,
+    status: "성능 개선",
+    content: `상세 내상세 내상세 내상세 내상세 내상세 내상세 내상세 내상세 내상세 내상세 내상세 내상세 내.`,
+  },
+];
+
+export const COREBOARDPOSTS: Post[] = [
+  {
+    id: 1,
+    title: "TestContainers docker 8초 -> 5초",
+    author: "한솔",
+    status: "성능 개선",
+    content: `상세 내상세 내상세 내상세 내상세 내상세 내용...`,
+  },
+  {
+    id: 2,
+    title: "공지: 포트폴리오 구조 안내",
+    author: "한솔",
     status: "트러블슈팅",
     content: `상세 내상세 내상세 내상세 내상세 내상세 내상세 내상세 내상세 내상세 내상세 내상세 내상세 내.`,
   },
